0a006189e649099189d461c00d78901eedd849d2
unknown <istofar201@gmail.com>
2022-08-13 23:21:02 +0100

    footer commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React from "react";
 import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    
+    //BEM
     <div className="App">
-      <Header />
-    <Home />
-      
-      
+      <Router>
+        <Header />
+        <Switch>
+          <Home />
+
+          <SearchPage />
+        </Switch>
+        <Footer />
+      </Router>
+
+      {/* Home */}
       {/*Banner*/}
       {/*Search*/}
 
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        2022 Airbnb Clone! No Rights Reserved - this is a demo! - i just need a
+        job
+      </p>
+      <p>Privacy • Terms • Sitemap • Company Details • 🥴</p>
+    </div>
+  );
+}
+
+export default Footer;
